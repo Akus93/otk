2b684689c1fa6e89d796773d5cc5bfef646c203e
Akus93 <dawid.rdzanek@protonmail.com>
2016-11-29 17:56:10 +0100

    Init commit

---
+++ scraper.py
@@ -0,0 +1,69 @@
+import re
+from bs4 import BeautifulSoup
+from requests import get
+from json import dump
+
+from pprint import pprint
+
+
+SITE_URL = 'http://www.mgsm.pl'
+
+phones = {}
+
+
+# pobieranie calej glownej strony
+main_site = get(SITE_URL).text
+main_site_soup = BeautifulSoup(main_site, 'html.parser')
+
+# uzupelnianie 'phones' markami telefonow i linkami do kazdej z nich
+brand_ul = main_site_soup.select_one('.Brands')
+for brand in brand_ul.select('li'):
+    a = brand.select_one('a')
+    phones[a.text] = {
+        'link': SITE_URL + a.attrs['href'],
+        'models': {}
+    }
+
+# zbieranie linkow do konkretnych modeli; zapisywane w phones[marka][models]
+for brand in phones.keys():
+    print('Zbieranie informacji o modelach telefonów marki {}...'.format(brand))
+
+    # pobieranie calej strony marki
+    brand_site = get(phones[brand]['link']).text
+    brand_site_soup = BeautifulSoup(brand_site, 'html.parser')
+
+    wrap_container = brand_site_soup.select_one('.wrapContainer')
+    phone_list_li = wrap_container.select('.phone-small-box-list')
+    for phone_li in phone_list_li:
+        phone_a = phone_li.select_one('a')
+        # zapisywanie modelu telefonu i linku do niego
+        if not phone_a.attrs['href'] == SITE_URL:
+            phones[brand]['models'][phone_a.text] = {
+                'link': SITE_URL + phone_a.attrs['href']
+            }
+
+
+# zbieranie danych o konkretnych modelach
+for brand in phones.keys():
+    for model in phones[brand]['models']:
+        print('Zbieranie informacji o telefonie {} {}...'.format(brand, model))
+
+        model_site = get(phones[brand]['models'][model]['link']).text
+        model_site_soup = BeautifulSoup(model_site, 'html.parser')
+
+        searched_spec = ['Standard GSM', 'Waga', 'Standardowa bateria', 'Wyświetlacz', 'Ochrona wyświetlacza',
+                         'Pamięć wbudowana', 'Pamięć RAM', 'System operacyjny', 'Procesor']
+        for spec in searched_spec:
+            try:
+                phones[brand]['models'][model][spec] = model_site_soup.find('div', text=re.compile(spec))\
+                                                        .parent.select_one('.phoneCategoryValue').text.strip().rstrip()
+            except AttributeError:
+                phones[brand]['models'][model][spec] = ''
+
+
+# zapisywanie informacji o telefonach do pliku
+dump(phones, open("phones.txt", 'w'))
+
+
+
+
