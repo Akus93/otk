f1ccd4e25adb4f0a853e10c49391610d514ce5be
akus5 <ania.kuszynska@gmail.com>
2017-01-24 14:18:02 +0100

    ontology

--- new_onto.py
+++ new_onto.py
@@ -96,34 +96,135 @@ class HaveQuickCharge(FunctionalProperty):
     range = [bool]
 
 
-my_phone = Smartphone("my_phone")
-nokia = Brand("Nokia")
-lumia6 = Model("Lumia_600")
-lumia7 = Model("Lumia_700")
-# # gsm = GSMStandard("600/900")
-# # weight = Weight("600 g")
-# # battery = Battery("2000 mAh")
-#
-# my_phone.HaveBrand.append(nokia)
-# nokia.HaveModel.append(lumia6)
-# nokia.HaveModel.append(lumia7)
-# # lumia6.HaveGSMStandard.append(gsm)
-# # lumia6.HaveWeight.append(weight)
-# # lumia6.HaveBattery.append(battery)
-# # lumia7.HaveGSMStandard.append(gsm)
-# # lumia7.HaveWeight.append(weight)
-# # lumia7.HaveBattery.append(battery)
-#
-lumia6.HaveGSMStandard = "600/900"
-lumia6.HaveWeight = "1000"
-lumia6.HaveBattery = "5000"
-lumia7.HaveGSMStandard = "600/900"
-lumia7.HaveWeight = "600"
-lumia7.HaveBattery = "2000"
-#
-# print(my_phone.HaveBrand)
-# print(nokia.HaveModel)
-# print(lumia7.HaveBattery)
-# print(my_phone)
+class HaveTouchScreen(FunctionalProperty):
+    ontology = onto
+    domain = [Model]
+    range = [bool]
 
-onto.save()
+
+class HaveAccelerometer(FunctionalProperty):
+    ontology = onto
+    domain = [Model]
+    range = [bool]
+
+
+class HaveProximitySensor(FunctionalProperty):
+    ontology = onto
+    domain = [Model]
+    range = [bool]
+
+
+class HaveLightSensor(FunctionalProperty):
+    ontology = onto
+    domain = [Model]
+    range = [bool]
+
+
+class HaveMagnetometer(FunctionalProperty):
+    ontology = onto
+    domain = [Model]
+    range = [bool]
+
+
+class HaveGyroscope(FunctionalProperty):
+    ontology = onto
+    domain = [Model]
+    range = [bool]
+
+
+class HaveBarometer(FunctionalProperty):
+    ontology = onto
+    domain = [Model]
+    range = [bool]
+
+
+class HaveAltimeter(FunctionalProperty):
+    ontology = onto
+    domain = [Model]
+    range = [bool]
+
+
+class HaveGravitySensor(FunctionalProperty):
+    ontology = onto
+    domain = [Model]
+    range = [bool]
+
+
+class HaveIrisScanner(FunctionalProperty):
+    ontology = onto
+    domain = [Model]
+    range = [bool]
+
+
+class HaveFingerprintReader(FunctionalProperty):
+    ontology = onto
+    domain = [Model]
+    range = [bool]
+
+
+class HaveThermometer(FunctionalProperty):
+    ontology = onto
+    domain = [Model]
+    range = [bool]
+
+
+class HaveHygrometer(FunctionalProperty):
+    ontology = onto
+    domain = [Model]
+    range = [bool]
+
+
+# # my_phone = Smartphone("my_phone")
+# # nokia = Brand("Nokia")
+# # lumia6 = Model("Lumia_600")
+# # lumia7 = Model("Lumia_700")
+# # # # gsm = GSMStandard("600/900")
+# # # # weight = Weight("600 g")
+# # # # battery = Battery("2000 mAh")
+# >>>>>>> Stashed changes
+# #
+# # my_phone.HaveBrand.append(nokia)
+# # nokia.HaveModel.append(lumia6)
+# # nokia.HaveModel.append(lumia7)
+# <<<<<<< Updated upstream
+# # # lumia6.HaveGSMStandard.append(gsm)
+# # # lumia6.HaveWeight.append(weight)
+# # # lumia6.HaveBattery.append(battery)
+# # # lumia7.HaveGSMStandard.append(gsm)
+# # # lumia7.HaveWeight.append(weight)
+# # # lumia7.HaveBattery.append(battery)
+# #
+# lumia6.HaveGSMStandard = "600/900"
+# lumia6.HaveWeight = "1000"
+# lumia6.HaveBattery = "5000"
+# lumia7.HaveGSMStandard = "600/900"
+# lumia7.HaveWeight = "600"
+# lumia7.HaveBattery = "2000"
+# =======
+# # # # lumia6.HaveGSMStandard.append(gsm)
+# # # # lumia6.HaveWeight.append(weight)
+# # # # lumia6.HaveBattery.append(battery)
+# # # # lumia7.HaveGSMStandard.append(gsm)
+# # # # lumia7.HaveWeight.append(weight)
+# # # # lumia7.HaveBattery.append(battery)
+# # #
+# # lumia6.HaveGSMStandard = "600/900"
+# # lumia6.HaveWeight = "1000"
+# # lumia6.HaveBattery = "5000"
+# # lumia7.HaveGSMStandard = "600/900"
+# # lumia7.HaveWeight = "600"
+# # lumia7.HaveBattery = "2000"
+# >>>>>>> Stashed changes
+# #
+# # print(my_phone.HaveBrand)
+# # print(nokia.HaveModel)
+# # print(lumia7.HaveBattery)
+# # print(my_phone)
+# <<<<<<< Updated upstream
+#
+# onto.save()
+# =======
+# #
+# #
+# # onto.save()
+# >>>>>>> Stashed changes
